b23aeadd754d98c23c368b7f30bb358bf3f5ca42
starosta2123 <[admin@mbou69.com]>
2022-12-09 14:49:38 +0400

    #Добавлена возможность отправки сообщений

--- app/src/main/java/com/example/chat/MainActivity.kt
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -2,17 +2,24 @@ package com.example.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chat.databinding.ActivityMainBinding
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        setContentView(R.layout.activity_main)
         // Write a message to the database
         val database = Firebase.database
-        val myRef = database.getReference("message").child("Dmitriy")
-
-        myRef.setValue("Hello, World!")
+        val myRef = database.getReference("message")
+        binding.bSend.setOnClickListener{
+            myRef.setValue(binding.edMessage.text.toString())
+        }
     }
 }
